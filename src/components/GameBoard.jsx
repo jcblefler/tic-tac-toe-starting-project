68f5cc7f794e5f900839be72274d91a9d8b4d7ee
Jacob Lefler <jcb.lefler@gmail.com>
2024-04-16 10:44:53 -0400

    project complete

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,8 +1,22 @@
-const initalGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-export default function GameBoard() {
-  return <ol id="game-board"></ol>;
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
